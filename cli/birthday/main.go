b9e72861476544a7cc126fe50954b8d8b51cb5d8
Eduardo ES Riccardi <ed@kukino.uk>
2021-03-01 19:33:14 +0000

    birthday nonces calculations

--- cli/birthday/main.go
+++ cli/birthday/main.go
@@ -9,19 +9,24 @@ import (
 // The following calculates the probability of duplicating a nonce when picked at random (as the current implementation
 // does). The current implementation uses 3 nonces of 12 bytes each (for each AES256/GCM cipher).
 //
-// The birthday problem is calculated for one nonce of 12 bytes (96 bits).
+// The probability of picking the same nonce twice (birthday paradox) is calculated for one nonce of 12 bytes (96 bits).
 //
-// After 100M nonces, the probability is:
 // 100M, 0.999999999999936891126951232322755154461737176572294203867910474082896986505554273464571044238582672950519...
-// or 13 decimal places, 100M writes using 1K blocks equals to writing 95 GiB
+// lower than 0.00000000000001, or 1 in 100.000.000.000.000
+// 100M blocks of 1K is approx. 95GiB written.
 //
-// 1000M, 0.99999999999368911275194093196384046656288207084848697587501906334095744742126709550393559064875913479644...
-// or 11 decimal places. after writing 950 GiB.
+// 1258M, 0.99999999999001261703522929514448885033012604685025065706759067812784429829344821159506777018445782306879...
+// lower than 0.000000000001, or  1 in 1.000.000.000.000
+// 1258M blocks of 1K is approx. 1.17TiB written.
 //
-// if you consider after breaking the first layer of AES256/GCM, there are another two more layers to go...
-// If we assume a whole file is written only once, an attacker can scan for duplicated two nonce in it, in practice
-// the blocks will be overwritten and the nonce replaced with new ones, an attacker will have to capture every byte
-// written to get a better chance of capturing two equal nonce.
+// 39805M, 0.9999999900007903323961553855263092697380031246078278537831527269974242016478462970070809347116269575979...
+// lower than 0.000000001 or 1 in 1.000.000.000 (1 billion)
+// 39805M block of 1K is approx. 37.07TiB written.
+//
+// You will have to write 37TiB into one file to have a chance of 1 in a billion to have a duplicated nonce, you also
+// have to consider it would compromise the first layer of AES256/GCM, there will be two more layers to break.
+//
+// see: `birthday.log.xz` for the dump of this program after running for about 50hs
 
 func main() {
 	const prec = 5000   // bits
